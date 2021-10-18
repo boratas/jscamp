4d7cf1e9f4254fcfa51ffad6aea295ae2912f40e
boratas <denizboratas@gmail.com>
2021-10-19 00:37:37 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+console.log("Merhaba Kodlama.io");
+
+// JS type safe değildir.
+
+let dolarBugun = 9.3;
+let dolarDun = 9.2;
+
+{
+  let dolarDun = 9.1;
+}
+
+console.log(dolarDun);
+
+//array
+
+let konutKredileri = [
+  "Konut kredisi",
+  "Emlak Konut Kredisi",
+  "Kamu Konut Kredisi",
+];
+
+console.log("<ul>");
+
+for (let i = 0; i < konutKredileri.length; i++) {
+  console.log("<li>" + konutKredileri[i] + "</li>");
+}
+
+console.log("</ul>");
+
+console.log(konutKredileri);
